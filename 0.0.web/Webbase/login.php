28a84a73bb4d312b53fd93e292b81042cd0c2799
marshadn <marshad@gmail.com>
2024-06-20 13:58:34 +0530

    modified

---
+++ 0.0.web/Webbase/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="">
+        <h1>Login Page</h1>
+        <input type="text" name="name" placeholder="Enter Username">
+    </form>
+</body>
+</html>
