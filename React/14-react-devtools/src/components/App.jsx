7d56f0cb4a539681ff436a9dce4fbc7732073f6d
Muhamed Marshad <muhamedmarshad18@gmail.com>
2024-06-25 14:13:29 +0530

    Add files

--- React/14-react-devtools/src/components/App.jsx
+++ React/14-react-devtools/src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar img="https://pbs.twimg.com/profile_images/744849215675838464/IH0FNIXk.jpg" />
+      <Avatar img="https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg" />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
